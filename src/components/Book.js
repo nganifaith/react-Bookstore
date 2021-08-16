dfc35bc1be6ce51c405a90d30d1ffbfbb218a423
ngani faith <nganifaith@gmail.com>
2021-08-16 23:01:59 +0100

    Add remove book option

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
+// eslint-disable-next-line react/prop-types
+const Book = ({ book, handleRemoveBook }) => (
   <tr>
     <td>{book.id}</td>
     <td>{book.title}</td>
     <td>{book.category}</td>
+    <td>
+      <button type="button" onClick={() => handleRemoveBook(book)}>
+        Remove book
+      </button>
+    </td>
   </tr>
 );
 
