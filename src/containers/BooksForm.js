55557baff1de900690a89d6d32cbbff342544ae6
ngani faith <nganifaith@gmail.com>
2021-08-16 20:48:24 +0100

    Add book list forms

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+const categories = [
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
+
 const BooksForm = () => (
-  <div>
-    <h1>Create Book</h1>
-    Submit here
-  </div>
+  <form>
+    <label htmlFor="title">
+      Title:
+      <input type="text" placeholder="Book title" name="title" />
+    </label>
+    <label htmlFor="categories">
+      <select id="categories" name="categories">
+        {categories.map((category) => (
+          <option value={category} key={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </label>
+    <button type="submit">Submit</button>
+  </form>
 );
 
 export default BooksForm;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,8 +2,23 @@ import React from 'react';
 
 const BooksList = () => (
   <div>
-    <h1>All Books</h1>
-    Submit here
+    <table>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Alice in Wonder Land</td>
+        <td>Adventure Novels</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Alice in the looking glass</td>
+        <td>Adventure Novels</td>
+      </tr>
+    </table>
   </div>
 );
 
