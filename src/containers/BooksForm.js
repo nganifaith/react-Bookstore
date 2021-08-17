901ea229e0d8cff67689adec20ab3c75f8c5a17f
ngani faith <nganifaith@gmail.com>
2021-08-17 23:53:29 +0100

    Add styles for bookform

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { createBook } from '../actions';
 import categoryData from '../categoryData';
-import '../css/bookForm.css';
 
 const BooksForm = () => {
   const dispatch = useDispatch();
@@ -17,27 +16,41 @@ const BooksForm = () => {
     e.target.reset();
   };
   return (
-    <div className="add_form">
-      <h1>ADD NEW BOOK</h1>
-      <form onSubmit={handleChange} className="add_book">
-        <label htmlFor="title">
-          <input required type="text" placeholder="Book title" name="title" />
-        </label>
-        <label htmlFor="categories">
-          <select id="categories" name="categories" defaultValue={' '}>
-            <option disabled value=" ">
-              {' '}
-            </option>
-            {categoryData.map((category) => (
-              <option value={category} key={category}>
-                {category}
+    <>
+      <span className="vertical-line-1" />
+      <div className="form-container">
+        <h2>ADD NEW BOOK</h2>
+        <form onSubmit={handleChange} className="form">
+          <label htmlFor="title">
+            <input
+              required
+              type="text"
+              className="form-control book-title"
+              placeholder="Book title"
+              name="title"
+            />
+          </label>
+          <label htmlFor="categories">
+            <select
+              id="categories"
+              name="categories"
+              className="book-category"
+              defaultValue={' '}
+            >
+              <option disabled value=" ">
+                {' '}
               </option>
-            ))}
-          </select>
-        </label>
-        <button type="submit">ADD BOOK</button>
-      </form>
-    </div>
+              {categoryData.map((category) => (
+                <option value={category} key={category}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </label>
+          <input type="submit" value="Add Book" className="submit-button" />
+        </form>
+      </div>
+    </>
   );
 };
 
